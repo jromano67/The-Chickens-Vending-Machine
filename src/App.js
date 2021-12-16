693ac08895a8c1f24520bc227c5a2c54249a44e9
jromano67 <89851864+jromano67@users.noreply.github.com>
2021-12-16 13:10:22 -0500

    added js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./Components/navbar.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
         <h1>The Chickens Vending Machine</h1>
+        <Navbar />
       </div>
     );
   }
---
+++ src/Components/navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        <b>The Chickens Vending Machine</b>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Order
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              About Us
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
